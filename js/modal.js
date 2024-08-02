9c3270703aa54b451dbd26ca3bf005d8cf61c5c9
Nina_Bolbot <n.v.bolbot@gmail.com>
2024-08-02 17:56:51 +0200

    hw6th

---
+++ js/modal.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+    html: document.querySelector("html"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-open");
+    refs.html.classList.toggle("modal-open");
+  }
+})();
